4000644689f1f2cca5b68f9c51c14d480670bafa
pat-s <patrick.schratz@gmail.com>
2019-12-01 18:25:56 +0100

    mlr3verseinfo: return a DF, add all core packages

--- R/mlr3verse_info.R
+++ R/mlr3verse_info.R
@@ -1,12 +1,12 @@
-#' @title Print package versions
+#' @title Print mlr3* package versions
 #'
 #' @description
-#' Returns the package versions of loaded mlr3 packages as [data.table()].
+#' Returns the package versions of loaded mlr3 packages as a [data.frame].
 #'
 #' @export
 #' @examples
 #' mlr3verse_info()
 mlr3verse_info = function() {
-  pkgs = c("mlr3", "mlr3tuning", "mlr3pipelines")
-  data.table(pkg = pkgs, version = mlr3misc::map_chr(pkgs, function(x) as.character(packageVersion(x))))
+  pkgs = c("mlr3", "mlr3tuning", "mlr3pipelines", "paradox", "mlr3filters", "mlr3db", "mlr3learners")
+  data.frame(Package = pkgs, Version = mlr3misc::map_chr(pkgs, function(x) as.character(packageVersion(x))))
 }
