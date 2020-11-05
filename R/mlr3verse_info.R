a60324f6c043a368656ee343feccba41740db147
pat-s <patrick.schratz@gmail.com>
2020-11-05 08:45:45 +0100

    add mlr3fselect

--- R/mlr3verse_info.R
+++ R/mlr3verse_info.R
@@ -7,9 +7,12 @@
 #' @export
 #' @examples
 #' mlr3verse_info()
-mlr3verse_info = function() {
-  pkgs = sort(c("mlr3", "bbotk", "mlr3tuning", "mlr3pipelines", "paradox", "mlr3filters",
-    "mlr3data", "mlr3learners", "mlr3viz"))
+mlr3verse_info <- function() {
+  pkgs <- sort(c(
+    "mlr3", "bbotk", "mlr3tuning", "mlr3pipelines", "paradox",
+    "mlr3filters", "mlr3fselect",
+    "mlr3data", "mlr3learners", "mlr3viz"
+  ))
   data.frame(Package = pkgs, Version = map_chr(pkgs, function(x) {
     as.character(utils::packageVersion(x))
   }))
---
+++ R/reexports_mlr3fselect.R
@@ -0,0 +1,7 @@
+#' @importFrom mlr3fselect mlr_fselectors
+#' @export
+mlr3fselect::mlr_fselectors
+
+#' @importFrom mlr3fselect fs
+#' @export
+mlr3fselect::fs
