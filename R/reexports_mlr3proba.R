93279350a26004b0affb323dd3c061cd5d1f7249
Michel Lang <michellang@gmail.com>
2022-05-06 09:11:59 +0200

    remove mlr3proba

--- R/reexports_mlr3proba.R
+++ R/reexports_mlr3proba.R
@@ -1,27 +1,27 @@
-#' @importFrom mlr3proba TaskSurv
-#' @export
-mlr3proba::TaskSurv
-
-#' @importFrom mlr3proba TaskDens
-#' @export
-mlr3proba::TaskDens
-
-#' @importFrom mlr3proba pecs
-#' @export
-mlr3proba::pecs
-
-#' @importFrom mlr3proba as_task_surv
-#' @export
-mlr3proba::as_task_surv
-
-#' @importFrom mlr3proba as_task_dens
-#' @export
-mlr3proba::as_task_dens
-
-#' @importFrom mlr3proba as_prediction_surv
-#' @export
-mlr3proba::as_prediction_surv
-
-#' @importFrom mlr3proba as_prediction_dens
-#' @export
-mlr3proba::as_prediction_dens
+# #' @importFrom mlr3proba TaskSurv
+# #' @export
+# mlr3proba::TaskSurv
+#
+# #' @importFrom mlr3proba TaskDens
+# #' @export
+# mlr3proba::TaskDens
+#
+# #' @importFrom mlr3proba pecs
+# #' @export
+# mlr3proba::pecs
+#
+# #' @importFrom mlr3proba as_task_surv
+# #' @export
+# mlr3proba::as_task_surv
+#
+# #' @importFrom mlr3proba as_task_dens
+# #' @export
+# mlr3proba::as_task_dens
+#
+# #' @importFrom mlr3proba as_prediction_surv
+# #' @export
+# mlr3proba::as_prediction_surv
+#
+# #' @importFrom mlr3proba as_prediction_dens
+# #' @export
+# mlr3proba::as_prediction_dens
