b67ebb251c79ce75b12a2baf46119f57223aac10
Michel Lang <michellang@gmail.com>
2023-04-13 10:35:01 +0200

    chore: remove unneeded requireNamespace() calls

--- R/zzz.R
+++ R/zzz.R
@@ -20,10 +20,6 @@
 "_PACKAGE"
 
 .onLoad = function(libname, pkgname) {
-  # these are not loaded automatically
-  requireNamespace("mlr3data", quietly = TRUE)
-  requireNamespace("mlr3learners", quietly = TRUE)
-
   # mlr3extralearners not on CRAN, only load if installed
   pkg = "mlr3extralearners"
   if (length(find.package(pkg, quiet = TRUE))) {
