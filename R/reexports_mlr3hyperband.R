fa477894e5f4e8575c3764650dc1f9ed4e8e9bd6
be-marc <marcbecker@posteo.de>
2023-04-20 10:29:26 +0200

    feat: add hyperband reexports

---
+++ R/reexports_mlr3hyperband.R
@@ -0,0 +1,11 @@
+#' @importFrom mlr3hyperband hyperband_budget
+#' @export
+mlr3hyperband::hyperband_budget
+
+#' @importFrom mlr3hyperband hyperband_n_configs
+#' @export
+mlr3hyperband::hyperband_n_configs
+
+#' @importFrom mlr3hyperband hyperband_schedule
+#' @export
+mlr3hyperband::hyperband_schedule
