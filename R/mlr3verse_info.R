b25fcb41605c2cac916bfb5b52ac7e2084fc2280
Sebastian Fischer <sebf.fischer@gmail.com>
2024-08-21 17:41:56 +0200

    update mlr3verse_info

--- R/mlr3verse_info.R
+++ R/mlr3verse_info.R
@@ -1,7 +1,8 @@
 #' @title Print mlr3* package versions
 #'
 #' @description
-#' Returns the package versions of imported mlr3 packages as a [data.table::data.table()].
+#' Returns the package versions of imported and suggested mlr3 packages as a [data.table::data.table()].
+#' Suggested packages that are not installed are listed with version `NA`.
 #'
 #' @importFrom mlr3misc map_chr
 #' @importFrom data.table data.table
@@ -9,12 +10,19 @@
 #' @examples
 #' mlr3verse_info()
 mlr3verse_info = function() {
-  imports = read.dcf(system.file("DESCRIPTION", package = "mlr3verse"), fields = "Imports")[[1L]]
-  imports = strsplit(imports, ",[[:space:]]*")[[1L]]
-  imports = sub("^([[:alnum:].]+).*", "\\1", imports)
-  imports = setdiff(imports, "data.table")
+  f = function(x) {
+    y = read.dcf(system.file("DESCRIPTION", package = "mlr3verse"), fields = x)[[1L]]
+    y = strsplit(y, ",[[:space:]]*")[[1L]]
+    sub("^([[:alnum:].]+).*", "\\1", y)
+  }
 
-  data.table(package = imports, version = map_chr(imports, function(x) {
-    as.character(utils::packageVersion(x))
-  }), key = "package")
+  imports = setdiff(f("Imports"), "data.table")
+  suggests = f("Suggests")
+  pkgs = c(imports, suggests)
+
+  versions = mlr3misc::map_chr(pkgs, function(pkg) {
+    tryCatch(as.character(utils::packageVersion(pkg)), error = function(e) NA_character_)
+  })
+
+  data.table(package = pkgs, version = versions, key = "package")
 }
