d39c4b7c2fe227e3c8f0c338ae464e362e8e7b7f
be-marc <marcbecker@posteo.de>
2025-01-14 13:35:08 +0100

    ...

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,5 @@
-#' @section Learn mlr3:
-#' * Book on mlr3: \url{https://mlr3book.mlr-org.com}
-#' * Use cases and examples gallery: \url{https://mlr3gallery.mlr-org.com}
-#' * Cheat Sheets: \url{https://github.com/mlr-org/mlr3cheatsheets}
-#'
-#' @section Additional Extension Packages:
-#' The \CRANpkg{mlr3verse} only installs the subset of packages which are on the
-#' one hand frequently required to perform a data analysis, and on the other
-#' hand do not pull in too many dependencies or system requirements.
-#'
-#' Additional extension packages include:
-#' * Analysis of benchmark experiments: \CRANpkg{mlr3benchmark}
-#' * Task interface for data bases: \CRANpkg{mlr3db}
-#' * Connector to [OpenML](https://www.openml.org): \CRANpkg{mlr3oml}
-#' * Additional learners: \url{https://github.com/mlr-org/mlr3extralearners}
-#' * Hyperband tuning algorithm: \CRANpkg{mlr3hyperband}
-#' * Spatio-temporal resampling: \CRANpkg{mlr3spatiotempcv}
-#'
 #' @import mlr3
+#' @import mlr3inferr
 "_PACKAGE"
 
 .onLoad = function(libname, pkgname) {
