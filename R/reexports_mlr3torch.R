8a48552f67685d7646a7e637d422926bec8d8908
Sebastian Fischer <sebf.fischer@gmail.com>
2026-08-21 09:13:00 +0200

    add mlr3torch

---
+++ R/reexports_mlr3torch.R
@@ -0,0 +1,67 @@
+#' @importFrom mlr3torch mlr3torch_callbacks
+#' @export
+mlr3torch::mlr3torch_callbacks
+
+#' @importFrom mlr3torch t_clbk
+#' @export
+mlr3torch::t_clbk
+
+#' @importFrom mlr3torch t_clbks
+#' @export
+mlr3torch::t_clbks
+
+#' @importFrom mlr3torch mlr3torch_losses
+#' @export
+mlr3torch::mlr3torch_losses
+
+#' @importFrom mlr3torch t_loss
+#' @export
+mlr3torch::t_loss
+
+#' @importFrom mlr3torch t_losses
+#' @export
+mlr3torch::t_losses
+
+#' @importFrom mlr3torch mlr3torch_optimizers
+#' @export
+mlr3torch::mlr3torch_optimizers
+
+#' @importFrom mlr3torch t_opt
+#' @export
+mlr3torch::t_opt
+
+#' @importFrom mlr3torch t_opts
+#' @export
+mlr3torch::t_opts
+
+#' @importFrom mlr3torch nn
+#' @export
+mlr3torch::nn
+
+#' @importFrom mlr3torch as_torch_callback
+#' @export
+mlr3torch::as_torch_callback
+
+#' @importFrom mlr3torch as_torch_callbacks
+#' @export
+mlr3torch::as_torch_callbacks
+
+#' @importFrom mlr3torch as_torch_loss
+#' @export
+mlr3torch::as_torch_loss
+
+#' @importFrom mlr3torch as_torch_optimizer
+#' @export
+mlr3torch::as_torch_optimizer
+
+#' @importFrom mlr3torch lazy_tensor
+#' @export
+mlr3torch::lazy_tensor
+
+#' @importFrom mlr3torch as_lazy_tensor
+#' @export
+mlr3torch::as_lazy_tensor
+
+#' @importFrom mlr3torch materialize
+#' @export
+mlr3torch::materialize
